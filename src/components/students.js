960a3333e2aa8902730e4fcfb635189419838460
catwhitmer <catwhitmer@gmail.com>
2020-06-03 19:58:36 -0400

    added create student method

--- src/components/students.js
+++ src/components/students.js
@@ -8,13 +8,33 @@ class Students {
 
     bindEventListeners() {
         this.studentsList = document.getElementById("students-container")
+        this.teacherId = document.getElementById("teacher_id")
+        this.studentName = document.getElementById("name")
+        this.studentHouse = document.getElementById("house")
+        this.studentBloodStatus = document.getElementById("blood_status")
+        this.studentPatronus = document.getElementById("patronus")
         this.studentForm = document.getElementById("student-form")
-        this.studentForm.addEventListener('submit', this.createStudent)
+        this.studentForm.addEventListener('submit', this.createStudent.bind(this))
     }
-
+    
     createStudent(e) {
         e.preventDefault()
-        console.log("student created")
+        const teacherIdValue = this.teacherId.value 
+        const nameValue = this.studentName.value 
+        const houseValue = this.studentHouse.value 
+        const bloodValue = this.studentBloodStatus.value 
+        const patronusValue = this.studentPatronus.value
+
+        this.service.createStudent(teacherIdValue, nameValue, houseValue, bloodValue, patronusValue).then(student => {
+            this.students.push(new Student(student))
+                this.teacherId.value = ''
+                this.studentName.value = ''
+                this.studentHouse.value = ''
+                this.studentBloodStatus.value = ''
+                this.studentPatronus.value = ''   
+                this.render()
+        })
+        
     }
 
     fetchStudents() {
