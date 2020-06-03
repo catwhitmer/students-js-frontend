a3b1efe11ba32d4278e46df495a4e1b6d693b110
catwhitmer <catwhitmer@gmail.com>
2020-06-03 13:20:27 -0400

    added the new student object creation

---
+++ src/components/student.js
@@ -0,0 +1,10 @@
+class Student {
+    constructor(studentJSON) {
+      this.id = studentJSON.id
+      this.name = studentJSON.name
+      this.house = studentJSON.house
+      this.blood_status = studentJSON.blood_status
+      this.patronus = studentJSON.patronus
+      this.teacher_id = studentJSON.teacher_id
+    }
+}
