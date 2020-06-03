34d091dcb0256fe55c84d88728f972d838d7a2ff
catwhitmer <catwhitmer@gmail.com>
2020-06-03 19:58:49 -0400

    added create student method

--- src/services/API.js
+++ src/services/API.js
@@ -6,4 +6,25 @@ class API {
     getStudents() {
       return fetch(this.baseUrl).then(resp => resp.json())
     }
+
+    getTeachers() {
+      return fetch("http://localhost:3000/api/v1/teachers").then(resp => resp.json())
+    }
+
+    createStudent(teacherIdValue, nameValue, houseValue, bloodValue, patronusValue) {
+      const student = {
+        teacher_id: teacherIdValue,
+        name: nameValue,
+        house: houseValue,
+        blood_status: bloodValue,
+        patronus: patronusValue
+    } 
+      return fetch(this.baseUrl,{
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ student }),
+      }).then(resp => resp.json())
+    }
 }
